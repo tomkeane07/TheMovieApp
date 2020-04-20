bd063944beb71d6bf0316d6662c552f95879da1a
tom keane <tomkeane07@gmail.com>
2020-04-20 01:43:23 +0100

    prepared movieListFragment to display parsed data

--- app/src/main/java/com/example/themovieapp/network/ResponseObject.kt
+++ app/src/main/java/com/example/themovieapp/network/ResponseObject.kt
@@ -1,8 +1,20 @@
 package com.example.themovieapp.network
 
+import com.squareup.moshi.Json
+
+
 data class ResponseObject(
     val page: Int,
     val results: List<Movie>,
     val total_results: Int,
     val total_pages: Int
+)
+
+
+//API info: https://developers.themoviedb.org/3/movies/get-top-rated-movies
+data class Movie(
+    val id: String,
+    val title: String,
+    val popularity: Double,
+    @Json(name = "img_src") val poster_path: String?
 )
